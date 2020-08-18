1931f67703b5da25e2719ec96d2ff76a12bcf24a
nothink <gakongakon@gmail.com>
2020-08-18 19:22:41 +0900

    use black

--- project/urls.py
+++ project/urls.py
@@ -19,12 +19,12 @@ from tutorial.quickstart import views
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
