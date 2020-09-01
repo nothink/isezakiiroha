74136f2b0348fd6cca92d809210a7afc436e0945
Satoshi Ishii <ishii@springboard-inc.jp>
2020-09-01 17:58:24 +0900

    doing tutorial 4

--- _tutorials/4_auth/snippets/views.py
+++ _tutorials/4_auth/snippets/views.py
@@ -4,54 +4,22 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 
 from django.contrib.auth.models import User
 
-# from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
-# class SnippetList(
-#     mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
-# ):
-#     """
-#     List all snippets, or create a new snippet.
-#     """
-
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-# class SnippetDetail(
-#     mixins.RetrieveModelMixin,
-#     mixins.UpdateModelMixin,
-#     mixins.DestroyModelMixin,
-#     generics.GenericAPIView,
-# ):
-#     """
-#     Retrieve, update or delete a snippet instance.
-#     """
-
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
-
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
