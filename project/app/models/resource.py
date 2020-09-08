6299d190b1eae70a8f64e9885755b0293cd0d830
nothink <gakongakon@gmail.com>
2020-09-09 06:07:21 +0900

    Changing resource models

---
+++ project/app/models/resource.py
@@ -0,0 +1,25 @@
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class Resource(models.Model):
+    """vcardサーバから取得したリソースを管理するモデルクラス"""
+
+    validate_safe_key = RegexValidator(
+        regex=r"^[0-9a-zA-Z:!/()*'._-]+$",
+        message=("This key isn't a safe key for S3/GCS."),
+    )
+
+    key = models.CharField(
+        max_length=1024,
+        null=False,
+        blank=False,
+        db_index=True,
+        editable=False,
+        unique=True,
+        validators=[validate_safe_key],
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.key
