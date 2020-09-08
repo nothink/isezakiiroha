40894dba8185c03f0beeaf5f13446ea71d8210f8
nothink <gakongakon@gmail.com>
2020-09-09 07:50:03 +0900

    update regex

--- project/app/models/resource.py
+++ project/app/models/resource.py
@@ -6,8 +6,12 @@ class Resource(models.Model):
     """vcardサーバから取得したリソースを管理するモデルクラス"""
 
     validate_safe_key = RegexValidator(
-        regex=r"^[0-9a-zA-Z:!/()*'._-]+$",
+        regex=r"^[0-9a-zA-Z:!/()*'.%@_-]+",
         message=("This key isn't a safe key for S3/GCS."),
+    )
+    vakudate_vcard_key = RegexValidator(
+        regex=r"^vcard/",
+        message=("This key isn't a vcard resource"),
     )
 
     key = models.CharField(
@@ -17,7 +21,7 @@ class Resource(models.Model):
         db_index=True,
         editable=False,
         unique=True,
-        validators=[validate_safe_key],
+        validators=[validate_safe_key, vakudate_vcard_key],
     )
     created = models.DateTimeField(auto_now_add=True)
 
