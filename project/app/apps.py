859a132df1c7a3d4c61458224b1882e69fb6aa75
nothink <gakongakon@gmail.com>
2020-09-13 16:33:46 +0900

    Prototype of adding GCS

--- project/app/apps.py
+++ project/app/apps.py
@@ -2,7 +2,10 @@ from django.apps import AppConfig
 
 
 class ApiAppConfig(AppConfig):
+    """API用のAppConfig"""
+
     name = "project.app"
 
     def ready(self):
+        """readyメソッドでsignalを読み込ませている"""
         from project.app import signals  # noqa
--- project/app/signals.py
+++ project/app/signals.py
@@ -1,19 +1,29 @@
+import io
 import urllib.request
 
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from google.cloud import storage
 
 from project.app.models import Resource
 
 
 @receiver(pre_save, sender=Resource)
-def create_user_profile(sender, instance, **kwargs):
-    # TODO: ここでfetchしてGCPに送りつける
-    print(instance.url)
+def fetch_and_upload_resource(sender, instance, **kwargs):
+    """
+    作成されたリソースをもとにリソース回収をはかり、GCSのbucketに格納する
+    """
 
-    req = urllib.request.Request(instance.url)
-    with urllib.request.urlopen(req) as res:
-        body = res.read()
-    print(len(body))
-    with open("tmp_gfiku.jpg", "wb") as f:
-        f.write(body)
+    try:
+        req = urllib.request.Request(instance.url)
+        with urllib.request.urlopen(req) as res:
+            body = res.read()
+        buf = io.BytesIO(body)
+        buf.seek(0)
+        client = storage.Client()
+        blob = client.bucket("irohasan").blob(instance.key)
+        blob.upload_from_file(buf)
+        instance.fetched = True
+    except Exception as e:
+        # なんらかのログを吐くべき
+        print(e)
