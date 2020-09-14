6e9f0c372e4ede1a304d36c5db0294be0de2d725
nothink <gakongakon@gmail.com>
2020-09-15 06:33:49 +0900

    Update pytest settings

--- _tutorials/1_serial/tutorial/snippets/views.py
+++ _tutorials/1_serial/tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
